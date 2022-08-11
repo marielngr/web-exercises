ca6cfed4df3ccdd8c24a931f2137bfe9550308e0
ThomasF85 <thomas.foeldi@gmail.com>
2022-08-11 13:57:22 +0200

    feat(js-kath-and-numbers): add new exercise for basic mathematical operators

---
+++ sessions/js-math-and-numbers/basic-operators/js/index.js
@@ -0,0 +1,52 @@
+console.clear();
+
+/*
+1: Uncomment the following two lines of code and write the missing function add
+*/
+// const sum = add(4, 5);
+// console.log('the sum is', sum);
+
+/*
+2: Uncomment the following two lines of code and write the missing function multiply
+*/
+// const product = multiply(4, 5);
+// console.log('the product is', product);
+
+/*
+3: Uncomment the following two lines of code and write the missing function substract
+*/
+// const difference = substract(4, 5);
+// console.log('the difference is', difference);
+
+/*
+4: Uncomment the following two lines of code and write the missing function divide
+*/
+// const quotient = divide(4, 5);
+// console.log('the quotient is', quotient);
+
+/*
+5: Uncomment the following two lines of code and write the missing function modulo
+*/
+// const remainder = modulo(6, 4);
+// console.log('the remainder is', remainder);
+
+/*
+6: Uncomment the following two lines of code and write the missing function decrement
+   The function should decrement a value (e.g. 42 decremented is 41)
+*/
+// const decreased = decrement(7);
+// console.log('decremented 7 = ', decreased);
+
+/*
+7: Uncomment the following two lines of code and write the missing function randomize
+   The function should return a random number between 1 and 10
+*/
+// const random = randomize();
+// console.log('a random number', random);
+
+/*
+8: Uncomment the following two lines of code and write the missing function average
+   The function should return the average of the 3 numbers given as arguments
+*/
+// const averageNumber = average(2, 17, 6);
+// console.log('the average value is', averageNumber);
